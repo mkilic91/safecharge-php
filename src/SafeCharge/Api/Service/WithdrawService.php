7d985aae5512f4140e9750dc28ef028a3ecb718c
mkilic <mustafa.kilic@beyn.com.tr>
2019-10-17 18:16:54 +0300

    added Withdraw service methods

--- src/SafeCharge/Api/Service/WithdrawService.php
+++ src/SafeCharge/Api/Service/WithdrawService.php
@@ -5,6 +5,7 @@ namespace SafeCharge\Api\Service;
 
 use SafeCharge\Api\Exception\ConfigurationException;
 use SafeCharge\Api\RestClient;
+use SafeCharge\Api\Utils;
 
 /**
  * Class WithdrawService
@@ -23,4 +24,347 @@ class WithdrawService extends BaseService
         $this->apiUrl = $this->client->getWithdraralUrl();
     }
 
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function approveRequest(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'userAccountId',
+            'operatorName',
+            'comment',
+            'successUrl',
+            'failUrl',
+            'selectedWdType',
+            'timeStamp',
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'approveRequest.do');
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function cancelRequest(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'userTokenId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'userTokenId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'userAccountId',
+            'timeStamp'
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'cancelRequest.do');
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function declineRequest(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'userAccountId',
+            'operatorName',
+            'comment',
+            'timeStamp'
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'declineRequest.do');
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function placeWithdrawalOrder(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'merchantUniqueId',
+            'userPMId',
+            'amount',
+            'currency',
+            'settlementType',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'merchantUniqueId',
+            'userPMId',
+            'amount',
+            'currency',
+            'settlementType',
+            'gwRelatedTransactionId',
+            'userAccountId',
+            'operatorName',
+            'comment',
+            'successUrl',
+            'failUrl',
+            'selectedWdType',
+            'timeStamp'
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'placeWithdrawalOrder.do');
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function sealRequest(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'userAccountId',
+            'operatorName',
+            'comment',
+            'timeStamp'
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'sealRequest.do');
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function submitRequest(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'userToken',
+            'userPMId',
+            'userDetails',
+            'amount',
+            'currency',
+            'merchantWDRequestId',
+            'merchantUniqueId',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'userTokenId',
+            'userPMId',
+            'userDetails',
+            'amount',
+            'currency',
+            'merchantWDRequestId',
+            'merchantUniqueId',
+            'userAccountId',
+            'customSiteName',
+            'customFieldX',
+            'successUrl',
+            'failUrl',
+            'selectedWdType',
+            'timeStamp',
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'submitRequest.do');
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function getCandidatesForRefund(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'timeStamp',
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'getCandidatesForRefund.do');
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     * @throws \SafeCharge\Api\Exception\ConnectionException
+     * @throws \SafeCharge\Api\Exception\ResponseException
+     * @throws \SafeCharge\Api\Exception\ValidationException
+     */
+    public function getRequests(array $params)
+    {
+        $mandatoryFields = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestId',
+            'timeStamp',
+            'checksum'
+        ];
+
+        $checksumParametersOrder = [
+            'merchantId',
+            'merchantSiteId',
+            'wdRequestOrderCount',
+            'userTokenId',
+            'userId',
+            'wdRequestId',
+            'merchantWDRequestId',
+            'merchantUniqueId',
+            'dateRange',
+            'statusList',
+            'stateList',
+            'paymentMethodsList',
+            'countryIsoList',
+            'amountRange',
+            'userRegistrationDateRange',
+            'sortField',
+            'sortOrder',
+            'firstResult',
+            'maxResults',
+            'operatorName',
+            'wdRequestOrderCountRange',
+            'merchantSiteIds',
+            'optionalWdType',
+            'timeStamp',
+        ];
+
+        $params = $this->appendMerchantIdMerchantSiteIdTimeStamp($params);
+
+        $params['checksum'] = Utils::calculateChecksum($params, $checksumParametersOrder, $this->client->getConfig()->getMerchantSecretKey(), $this->client->getConfig()->getHashAlgorithm());
+
+        $this->validate($params, $mandatoryFields);
+
+        return $this->requestJson($params, 'getCandidatesForRefund.do');
+    }
 }
